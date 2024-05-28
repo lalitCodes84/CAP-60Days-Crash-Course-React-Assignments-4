5cdca653cac8c1dac80a079a523821133010b319
lalitCodes84 <vickysharma71za@gmail.com>
2024-05-28 23:26:59 +0530

    Crash Course Final Assignment 4 Accomplished

---
+++ Crash Course Final Assignment/src/components/pages/TicketEdit.jsx
@@ -0,0 +1,7 @@
+function TicketEdit(){
+    return(
+        <h1 style={{textAlign: "center", fontSize:"20px" ,fontWeight:"bolder",backgroundColor:"crimson", color:"white"}}>TicketEdit</h1>
+    )
+
+}
+export default TicketEdit
